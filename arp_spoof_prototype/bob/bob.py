f696abcd96ede0c89dd6b88b45f333b2ea594ad8
Victor Nate Graf <vnategraf@gmail.com>
2017-01-19 11:30:33 -0600

    Typo fix

--- arp_spoof_prototype/bob/bob.py
+++ arp_spoof_prototype/bob/bob.py
@@ -13,7 +13,7 @@ while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print("received message:", data)
         if flag in str(data):
-            sock.sendto("Yup, that's is!".encode('utf-8'), addr)
+            sock.sendto("Yup, that's it!".encode('utf-8'), addr)
             print("Correct flag recieved")
         else:
             sock.sendto("Nope".encode('utf-8'), addr)
