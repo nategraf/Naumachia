59dfeb34a19133bcc9532a0ae387b9da8d08fe12
Victor "Nate" Graf <nategraf1@gmail.com>
2017-11-12 21:36:56 -0600

    add arg parsing to render script

--- render-docker-compose.py
+++ render-docker-compose.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
 import jinja2
-import os
 import yaml
+import argparse
+from os import path
 
-TEMPLATE = "docker-compose.yml.j2"
-CONFIG = "config.yml"
-RENDERED = "docker-compose.yml"
+def parse_args():
+    dir = path.dirname(__file__)
+
+    parser = argparse.ArgumentParser(
+            description='Parse the Naumachia config file and set up the environment',
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--config', metavar="PATH", default=path.join(dir, 'config.yml'), help='path to Naumachia config file')
+    parser.add_argument('--template', metavar="PATH", default=path.join(dir, 'docker-compose.yml.j2'), help='path to the docker-compose template')
+    parser.add_argument('--rendered', metavar="PATH", default=path.join(dir, 'docker-compose.yml'), help='path to the rendered docker-compose output')
+
+    return parser.parse_args()
 
 def render(tpl_path, context):
-    path, filename = os.path.split(tpl_path)
+    dirname, filename = path.split(tpl_path)
     return jinja2.Environment(
-        loader=jinja2.FileSystemLoader(path or './')
+        loader=jinja2.FileSystemLoader(dirname or './')
     ).get_template(filename).render(context)
 
 if __name__ == "__main__":
-    dir = os.path.dirname(__file__)
-    template_path = os.path.abspath(os.path.join(dir, TEMPLATE))
-    config_path = os.path.abspath(os.path.join(dir, CONFIG))
-    rendered_path = os.path.abspath(os.path.join(dir, RENDERED))
+    args = parse_args()
 
     context = None
-    with open(config_path, 'r') as config_file:
+    with open(args.config, 'r') as config_file:
         context = yaml.load(config_file)
 
-    with open(rendered_path, 'w') as rendered_file:
-        rendered_file.write(render(template_path, context))
+    with open(args.rendered, 'w') as rendered_file:
+        rendered_file.write(render(args.template, context))
 
-    print("{} rendered from {} with context from {}".format(RENDERED, TEMPLATE, CONFIG))
+    print("{} rendered from {} with context from {}".format(args.rendered, args.template, args.config))
