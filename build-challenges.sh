286e8795aecaebfe0607f5dc29a50f33ff7c698d
Victor "Nate" Graf <nategraf1@gmail.com>
2018-03-15 23:08:20 -0500

    add build script for the lazy

---
+++ build-challenges.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+for file in $(find $SCRIPT_DIR/challenges -name docker-compose.yml); do
+    pushd $(dirname $file)
+    sudo docker-compose build $@
+    popd
+done
