d271ed30580bce139ad15a63e860a9af78db5c63
Victor "Nate" Graf <nategraf1@gmail.com>
2018-04-13 22:48:28 -0700

    Add OpenVpn class for managing tunnels

--- test/strategies/ip.py
+++ test/strategies/ip.py
@@ -1,7 +1,8 @@
-import socket
 import fcntl
-import struct
 import operator
+import socket
+import struct
+import warnings
 
 class Ip:
     _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -53,3 +54,72 @@ class Ip:
     @classmethod
     def ifmask(cls, ifname):
         return cls._ifctl(ifname, 0x891b)  # SIOCGIFNETMASK
+
+class OpenVpnError(Exception):
+    def __init__(self, instance, msg):
+        self.instance = instance
+        super().__init__(msg)
+
+class OpenVpn:
+    exe = 'openvpn'
+    initmsg = b'Initialization Sequence Completed'
+
+    def __init__(self, **kwargs):
+        if 'daemonize' in kwargs:
+            warnings.warn("This class will not be able to close a daemonized tunnel", warnings.Warning)
+
+        self.options = kwargs
+        self.initialized = False
+        self._process = None
+
+    def args(self):
+        result = []
+        for name, value in self.options.items():
+            result.append('--{!s}'.format(name))
+
+            # None is special to indicate the option have no value
+            if value is not None:
+                result.append(str(value))
+        return result
+
+    def check(self):
+        if self._process is not None:
+            self._process.poll()
+            code = self._process.returncode
+            if code is not None and code != 0:
+                raise OpenVpnError(self, "OpenVPN tunnel exited with error code: {:d}")
+
+    def running(self):
+        return self._process is not None and self._process.poll() is None
+
+    def connect(self):
+        if not self.running():
+            self.initialized = False
+            self._process = subprocess.Popen(
+                [self.exe] + self.args(),
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            self.check()
+
+    def disconnect(self):
+        if self.running():
+            self._process.terminate()
+
+    def waitforinit(self):
+        if not self.initialized:
+            for line in self._process.stdout:
+                if self.initmsg in line:
+                    self.initialized = True
+                    break
+            else:
+                self.check()
+                raise OpenVpnError(self, "OpenVPN exited with code 0, but did not display init msg")
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.disconnect()
