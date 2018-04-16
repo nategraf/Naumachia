e89402e12ef38b492bb2d7aafce8f37b4ff92c52
Victor "Nate" Graf <nategraf1@gmail.com>
2018-04-16 18:22:27 -0500

    generalize the notion of address and add MAC address support

--- test/worker/net.py
+++ test/worker/net.py
@@ -5,38 +5,54 @@ import struct
 import warnings
 import subprocess
 import logging
+import base64
 
 logger = logging.getLogger(__name__)
 
-class Ip:
-    # both in packed bytes form
-    def __init__(self, ip):
+# Dummy socket used for fcntl functions
+_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class AddrMeta(type):
+    @property
+    def maxvalue(cls):
+        return (0x1 << (cls.bytelen * 8)) - 1
+
+class Addr(metaclass=AddrMeta):
+    bytelen = 0
+
+    def __init__(self, addr):
         self._str = None
         self._int = None
         self._bytes = None
 
-        if isinstance(ip, str):
-            self._str = ip
-        elif isinstance(ip, int):
-            self._int = ip
-        elif isinstance(ip, bytes):
-            if len(ip) == 4:
-                self._bytes = ip
+        if isinstance(addr, type(self)):
+            self._str = addr._str
+            self._bytes = addr._bytes
+            self._int = addr._int
+        elif isinstance(addr, str):
+            self._str = addr
+        elif isinstance(addr, int):
+            self._int = addr
+        elif isinstance(addr, bytes):
+            if len(addr) == self.bytelen:
+                self._bytes = addr
             else:
-                self._str = ip.decode('utf-8')
+                self._str = addr.decode('utf-8')
+        else:
+            raise ValueError('Cannot create {!s} from {!s}'.format(type(self), type(addr)))
 
     # Operations
     def __and__(self, other):
-        return self.__class__(int(self) & int(other))
+        return type(self)(int(self) & int(other))
 
     def __or__(self, other):
-        return self.__class__(int(self) | int(other))
+        return type(self)(int(self) | int(other))
 
     def __xor__(self, other):
-        return self.__class__(int(self) ^ int(other))
+        return type(self)(int(self) ^ int(other))
 
     def __invert__(self):
-        return self.__class__(int(self) ^ 0xFFFFFFFF)
+        return type(self)(int(self) ^ self.maxvalue)
 
     # Conversions
     def __str__(self):
@@ -45,48 +61,62 @@ class Ip:
         return self._str
 
     def __int__(self):
-        return struct.unpack('!I', bytes(self))[0]
+        return int.from_bytes(bytes(self), byteorder='big')
 
     def __bytes__(self):
         if self._bytes is None:
             if self._str is not None:
                 self._bytes = socket.inet_aton(self._str)
             elif self._int is not None:
-                self._bytes = struct.pack('!I', self._bytes)
+                self._bytes = self._int.to_bytes(self.bytelen, byteorder='big')
         return self._bytes
 
     def __repr__(self):
-        return '<{0}.{1} {2!s}>'.format(__name__, self.__class__.__name__, self)
+        return '<{0}.{1} {2!s}>'.format(__name__, type(self).__name__, self)
+
+class Ip(Addr):
+    bytelen = 4
 
     def slash(self):
         x, i = int(self), 0
         while x & 0x1 == 0:
             x >>= 1
             i += 1
-        return i
+        return 32 - i
 
-_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+class Mac:
+    bytelen = 6
 
 def _ifctl(ifname, code):
     if isinstance(ifname, str):
         ifname = ifname.encode('utf-8')
 
-    ip = fcntl.ioctl(
+    return fcntl.ioctl(
         _socket.fileno(),
         code,
         struct.pack('256s', ifname[:15])
-    )[20:24]
-
-    return Ip(ip)
+    )
 
 def ifaddr(ifname):
-    return Ip._ifctl(ifname, 0x8915) # SIOCGIFADDR
+    return Ip(_ifctl(ifname, 0x8915)[20:24]) # SIOCGIFADDR
 
 def ifmask(ifname):
-    return Ip._ifctl(ifname, 0x891b)  # SIOCGIFNETMASK
+    return Ip(_ifctl(ifname, 0x891b)[20:24]) # SIOCGIFNETMASK
+
+def ifhwaddr(ifname):
+    return Mac(_ifctl(ifname, 0x8927)[18:24]) # SIOCGIFHWADDR
 
 def cidr(ip, mask):
     return "{!s}/{:d}".format(ip, mask.slash())
+
+def parsecidr(ipnet):
+    ipstr, maskstr = ipnet.split('/')
+    ip = Ip(ipstr)
+    mask = Ip(0xffffffff ^ ((0x00000001 << (32-int(maskstr)))-1))
+    return ip, mask
+
+def ifcidr(ifname):
+    return cidr(ifaddr(ifname), ifmask(ifname))
 
 class OpenVpnError(Exception):
     def __init__(self, instance, msg):
